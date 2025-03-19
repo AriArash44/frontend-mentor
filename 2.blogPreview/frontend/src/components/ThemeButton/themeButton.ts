04ff7ed3f9ed6274c0bca1f7f309ba7fa1fb2ba9
AraArash44 <arashasghari1245@gmail.com>
2025-03-19 19:39:16 +0330

    add theme button components

--- 2.blogPreview/frontend/src/components/ThemeButton/themeButton.ts
+++ 2.blogPreview/frontend/src/components/ThemeButton/themeButton.ts
@@ -1,33 +1,26 @@
+import themeButtonTemplateRaw from './themeButton.html?raw';
+import themeButtonStyles from './themeButton.css?raw';
+import bootstrapCss from 'bootstrap/dist/css/bootstrap.min.css?raw';
+
 class ThemeButtonComponent extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
-        Promise.all([
-            this.loadCSS('themeButton.css'),
-            this.loadHTML('themeButton.html'),
-        ]).then(([cssContent, htmlContent]) => {
-            const style = document.createElement('style');
-            style.textContent = cssContent;
-            shadow.appendChild(style);
-            const wrapper = document.createElement('div');
-            wrapper.innerHTML = htmlContent;
-            const button = wrapper.querySelector('button');
-            if (button?.getAttribute('active') === 'true') {
-                button.classList.add('but-active');
-            }
-            shadow.appendChild(wrapper);
-        });
-    }
-
-    private async loadCSS(file: string): Promise<string> {
-        return fetch(file).then((response) => response.text());
-    }
-
-    private async loadHTML(file: string): Promise<string> {
-        return fetch(file).then((response) => response.text());
+        const style = document.createElement('style');
+        style.textContent = bootstrapCss.concat(themeButtonStyles);
+        shadow.appendChild(style);
+        const wrapper = document.createElement('div');
+        wrapper.innerHTML = themeButtonTemplateRaw;
+        const button = wrapper.querySelector('button');
+        if (this.getAttribute('active') === 'true') {
+            button?.classList.add('border-4');
+        }
+        const theme = this.getAttribute('color');
+        button?.classList.add("bg-" + theme);
+        shadow.appendChild(wrapper);
     }
 }
 
-customElements.define('theme-bottun-component', ThemeButtonComponent);
+customElements.define('theme-button-component', ThemeButtonComponent);
 
 export default ThemeButtonComponent;
