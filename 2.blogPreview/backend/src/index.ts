9ff09c22f0697c8724221c0d93db9e803ab7e5fe
AraArash44 <arashasghari1245@gmail.com>
2025-03-24 15:24:14 +0330

    fix cors credential issue

--- 2.blogPreview/backend/src/index.ts
+++ 2.blogPreview/backend/src/index.ts
@@ -15,7 +15,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 
 const server = http.createServer(app);
 setupWebSocket(server);
