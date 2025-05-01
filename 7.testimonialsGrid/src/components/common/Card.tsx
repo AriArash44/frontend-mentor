67aa121a3569a770783390f6557a04ebb2bbdb54
AraArash44 <arashasghari1245@gmail.com>
2025-05-01 19:43:03 +0330

    fix some minor styling isseus

--- 7.testimonialsGrid/src/components/common/Card.tsx
+++ 7.testimonialsGrid/src/components/common/Card.tsx
@@ -9,21 +9,24 @@ interface CardProps {
     caption: string,
     bgColor: string,
     primaryColor: string,
-    secondaryColor: string
+    secondaryColor: string,
+    borderColor: string,
+    qutation?: boolean
 }
 
 const Card = (props: CardProps) => {
     return (
-      <section className={`bg-${props.bgColor} rounded-lg p-7 h-full`}>
-        <div className="flex gap-5">
-          <img className="avatar" src={props.avatar} alt={props.alt}/>
+      <section className={`bg-${props.bgColor} rounded-lg p-11 h-full custom-shadow relative`}>
+        {props.qutation ? <img className="absolute top-0 right-20 w-35" src="/images/bg-pattern-quotation.svg" alt=""/>: ""}
+        <div className="flex align-end gap-5 relative z-10">
+          <img className={`avatar border-${props.borderColor}`} src={props.avatar} alt={props.alt}/>
           <div>
-            <h3 className={`text-${props.primaryColor}`}>{props.name}</h3>
-            <p className={`text-${props.secondaryColor}`}>{props.status}</p>
+            <h3 className={`text-${props.primaryColor} font-medium`}>{props.name}</h3>
+            <p className={`text-${props.secondaryColor} font-medium`}>{props.status}</p>
           </div>
         </div>
-        <h2 className={`text-${props.primaryColor} my-3`}>{props.header}</h2>
-        <p className={`text-${props.secondaryColor}`}>"{props.caption}"</p>
+        <h2 className={`text-${props.primaryColor} my-3 font-semibold relative z-10`}>{props.header}</h2>
+        <p className={`text-${props.secondaryColor} font-medium`}>"{props.caption}"</p>
       </section>
     );
 };
