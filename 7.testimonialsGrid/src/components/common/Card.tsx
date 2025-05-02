7b8db775dd44330fd7bbeb96f795124dd407f7eb
AraArash44 <arashasghari1245@gmail.com>
2025-05-02 14:06:45 +0330

    add animation for fading

--- 7.testimonialsGrid/src/components/common/Card.tsx
+++ 7.testimonialsGrid/src/components/common/Card.tsx
@@ -1,5 +1,6 @@
 import { graduateStatus } from "../../types/graduateStatus";
 import { useCardsVisibility } from "../../hooks/useCardsVisibility";
+import { motion, AnimatePresence } from "framer-motion"; 
 
 interface CardProps {
     avatar: string,
@@ -19,18 +20,32 @@ interface CardProps {
 const Card = (props: CardProps) => {
     const { cardsVisibilityState } = useCardsVisibility();
     return (
-      <section className={`bg-${props.bgColor} rounded-lg p-11 h-full custom-shadow relative ${cardsVisibilityState[props.index] ? "" : "invisible"}`}>
-        {props.qutation ? <img className="absolute top-0 right-20 w-35" src="/images/bg-pattern-quotation.svg" alt=""/>: ""}
-        <div className="flex align-end gap-5 relative z-10">
-          <img className={`avatar border-${props.borderColor}`} src={props.avatar} alt={props.alt}/>
-          <div>
-            <h3 className={`text-${props.primaryColor} font-medium`}>{props.name}</h3>
-            <p className={`text-${props.secondaryColor} font-medium`}>{props.status}</p>
-          </div>
-        </div>
-        <h2 className={`text-${props.primaryColor} my-3 font-semibold relative z-10`}>{props.header}</h2>
-        <p className={`text-${props.secondaryColor} font-medium`}>"{props.caption}"</p>
-      </section>
+      <AnimatePresence>
+        {cardsVisibilityState[props.index] && (
+          <motion.div
+            key={props.index}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            layout
+            className="h-full"
+          >
+            <section className={`bg-${props.bgColor} rounded-lg p-11 h-full custom-shadow relative`}>
+              {props.qutation ? <img className="absolute top-0 right-1/12 md:right-1/8 w-30 md:w-35" src="/images/bg-pattern-quotation.svg" alt=""/>: ""}
+              <div className="flex align-end gap-5 relative z-10">
+                <img className={`avatar border-${props.borderColor}`} src={props.avatar} alt={props.alt}/>
+                <div>
+                  <h3 className={`text-${props.primaryColor} font-medium`}>{props.name}</h3>
+                  <p className={`text-${props.secondaryColor} font-medium`}>{props.status}</p>
+                </div>
+              </div>
+              <h2 className={`text-${props.primaryColor} my-3 font-semibold relative z-10`}>{props.header}</h2>
+              <p className={`text-${props.secondaryColor} font-medium`}>"{props.caption}"</p>
+            </section>
+          </motion.div>
+        )}
+      </AnimatePresence>
     );
 };
 
