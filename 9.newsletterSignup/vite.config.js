9ee34d15596775fcd7ba7e0e2c24c2491c79898f
AraArash44 <arashasghari1245@gmail.com>
2025-05-07 23:14:17 +0330

    initiallized project 9

---
+++ 9.newsletterSignup/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/frontend-mentor/9.newsletterSignup/' : '/',
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+}))
