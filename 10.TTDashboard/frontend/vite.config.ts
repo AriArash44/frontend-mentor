c66269010fbd450ffe04b661ba1f0dbb10b79208
AraArash44 <arashasghari1245@gmail.com>
2025-05-12 11:45:46 +0330

    add assets of ttDashboard

--- 10.TTDashboard/frontend/vite.config.ts
+++ 10.TTDashboard/frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/frontend-mentor/10.ttDashboard/' : '/',
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+}))
