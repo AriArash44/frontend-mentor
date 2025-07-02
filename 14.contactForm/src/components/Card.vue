ac63ebe11ff8bb36d8f10df8ea9469b48ce5e048
AraArash44 <arashasghari1245@gmail.com>
2025-07-02 18:14:44 +0330

    fix layout and Card component

--- 14.contactForm/src/components/Card.vue
+++ 14.contactForm/src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white p-4 rounded-2xl w-11/12">
+  <div class="bg-white p-4 rounded-lg m-auto shadow-sm w-11/12 md:w-176">
     <slot></slot>
   </div>
 </template>
--- 14.contactForm/src/views/CFView.vue
+++ 14.contactForm/src/views/CFView.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+    import CFLayout from '../layouts/CFLayout.vue';
+</script>
+
 <template>
-    hgfds
+  <CFLayout>
+    <template #header>
+      <h1 class="sr-only hidden">contact form task from frontend mentor</h1>
+    </template>
+    <template #main>
+      mj
+    </template>
+  </CFLayout>
 </template>
