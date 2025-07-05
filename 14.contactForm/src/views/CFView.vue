7a6b747da790709e034fe22f93fb5b12757a848a
AraArash44 <arashasghari1245@gmail.com>
2025-07-05 18:36:30 +0330

    add textarea and checkbox into the form

--- 14.contactForm/src/views/CFView.vue
+++ 14.contactForm/src/views/CFView.vue
@@ -14,7 +14,9 @@
         isFirstNameValid: false,
         isLastNameValid: false,
         isEmailValid: false,
-        isQyeryTypeSelected: '',    
+        isQyeryTypeSelected: '',
+        isMessageWritten: false,
+        isConsented: false
     })
 </script>
 
@@ -30,8 +32,23 @@
         <Input title="Last Name" :regex="/^(?!\s*$).+/" error="This feild is required" v-model:isCorrect="isFormValid.isLastNameValid" />
         <Input title="Email Address" :regex="/^[^\s@]+@[^\s@]+\.[^\s@]+$/" error="Please enter a valid email address" 
         v-model:isCorrect="isFormValid.isEmailValid" class="md:col-span-2 md:mt-2" />
+        <div class="flex gap-1 md:col-span-2">
+          <p>Query Type</p>
+          <p class="text-green-600">*</p>
+        </div>
         <RadioButton v-model="isFormValid.isQyeryTypeSelected" label="General Enquiry" value="General Enquiry" />
         <RadioButton v-model="isFormValid.isQyeryTypeSelected" label="Support Request" value="Support Request" />
+        <div class="flex gap-1 md:col-span-2">
+          <p>Message</p>
+          <p class="text-green-600">*</p>
+        </div>
+        <textarea :class="['border-1 border-gray-500 md:col-span-2 outline-0 p-1 rounded-lg resize-none h-25', 
+        isFormValid.isQyeryTypeSelected ? '' : '' ]"></textarea>
+        <div class="flex justify-start gap-2 md:col-span-2">
+          <input id="consentCheckbox" type="checkbox" />
+          <label for="consentCheckbox">I consent to being contacted by the team</label>
+          <p class="text-green-600">*</p>
+        </div>
         <button class="bg-green-600 text-white py-2 rounded-md cursor-pointer md:col-span-2" 
           @click.prevent="toastOnce">Submit</button>
       </div>
