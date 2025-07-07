0440ddfb742f9764aac0ad311d3a3a08fd77e0f8
AraArash44 <arashasghari1245@gmail.com>
2025-07-07 11:36:11 +0330

    improve focusing and selecting by keyboard for accessibility

--- 14.contactForm/src/components/RadioButton.vue
+++ 14.contactForm/src/components/RadioButton.vue
@@ -9,8 +9,8 @@
 </script>
 
 <template>
-  <button :class="['flex items-center gap-2 border-1 border-gray-500 rounded-lg p-2 px-4 cursor-pointer',
-  selected === value ? 'bg-green-200 border-green-600' : '']" @click="selected = value">
+  <button :class="['flex items-center gap-2 border-1 border-gray-500 rounded-lg p-2 px-4 cursor-pointer', selected === value ? 'bg-green-200 border-green-600' : '']"
+  type="button" @click="selected = value" @keydown.enter.prevent="selected = value" @keydown.space.prevent="selected = value">
     <input
       :id="radioId"
       type="radio"
