c0ac00cdeb350d98849e4d4635ca30c92bf3f149
AraArash44 <arashasghari1245@gmail.com>
2025-08-16 19:38:37 +0330

    fix structure of the project

--- 15.newsHomePage/tailwind.config.js
+++ 15.newsHomePage/tailwind.config.js
@@ -4,13 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        softOrange: 'hsl(35, 0.77, 0.62)',
-        softRed: 'hsl(5, 0.85, 0.63)',
+        'soft-orange': 'hsl(35, 0.77, 0.62)',
+        'soft-red': 'hsl(5, 0.85, 0.63)',
         neutral: {
-          offWhite: 'hsl(36, 1, 0.99)',
-          grayishBlue: 'hsl(233, 0.08, 0.79)',
-          darkGrayishBlue: 'hsl(236, 0.13, 0.42)',
-          veryDarkBlue: 'hsl(240, 1, 0.05)'
+          'off-white': 'hsl(36, 1, 0.99)',
+          'grayish-blue': 'hsl(233, 0.08, 0.79)',
+          'dark-grayish-blue': 'hsl(236, 0.13, 0.42)',
+          'very-dark-blue': 'hsl(240, 1, 0.05)'
         }
       }
     },
