678a5b44e3e225b7004cb2c9ef8c55208032ff35
Arash44 <arashasghari1245@gmail.com>
2025-08-28 23:25:55 +0330

    add thumbnail component

---
+++ 16.eProductPage/src/lib/components/thumbnail.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let images: string[] = [];
+    export let index = 0;
+</script>
+
+<div class="flex items-center w-2/3 m-auto gap-4">
+    {#each images as img, i}
+        <button class={`cursor-pointer bg-white rounded-xl overflow-clip ${index === i ? 'border-2 border-c-orange' : ''}`} on:click={() => {index = i}}>
+            <img class={`hover:opacity-70 ${index === i ? 'opacity-40' : ''}`} src={img} alt={`product-img-${i}`} />
+        </button>
+    {/each}
+</div>
--- 16.eProductPage/src/routes/+page.svelte
+++ 16.eProductPage/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
     import Modal from "$lib/components/modal.svelte";
     import Carousel from "$lib/components/carousel.svelte";
+    import Thumbnail from "$lib/components/thumbnail.svelte";
     let index = 0;
+    const productImages = ["/images/product-1.jpg", "/images/product-2.jpg", "/images/product-3.jpg", "/images/product-4.jpg"];
+    const thumbnailImages = ["/images/product-1-thumbnail.jpg", "/images/product-2-thumbnail.jpg", "/images/product-3-thumbnail.jpg", "/images/product-4-thumbnail.jpg"];
 </script>
 
 
 <Modal>
-    <Carousel bind:index images={["/images/product-1.jpg", "/images/product-2.jpg", "/images/product-3.jpg", "/images/product-4.jpg"]} />
+    <Carousel bind:index images={productImages} />
+    <Thumbnail bind:index images={thumbnailImages} />
 </Modal>
