90d02a62da7560587c9fc51916c3f5763475aba2
Arash44 <arashasghari1245@gmail.com>
2025-09-13 15:30:41 +0330

    fix base URL for deploying on vercel

--- 16.eProductPage/svelte.config.js
+++ 16.eProductPage/svelte.config.js
@@ -1,16 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/frontend-mentor/16.EProductPage'
-		}
 	}
 };
 
