54e714f481de84ffeb0d98fa59e6e2c5a30c8153
Arash44 <arashasghari1245@gmail.com>
2025-11-04 01:10:43 +0330

    fix postcss issues

--- 17.nft-card/postcss.config.mjs
+++ 17.nft-card/postcss.config.mjs
@@ -1,5 +1,27 @@
+// during build: 
+// "@tailwindcss/postcss"
+// during build-css
+// {
+//   postcssPlugin: 'remove-unsupported-at-rules',
+//   AtRule: {
+//     layer: (atRule) => atRule.remove(),
+//     property: (atRule) => atRule.remove(),
+//     supports: (atRule) => atRule.remove(),
+//   },
+// },
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: [
+    "@tailwindcss/postcss"
+    // {
+    //   postcssPlugin: 'remove-unsupported-at-rules',
+    //   AtRule: {
+    //     layer: (atRule) => atRule.remove(),
+    //     property: (atRule) => atRule.remove(),
+    //     supports: (atRule) => atRule.remove(),
+    //   },
+    // },
+  ],
 };
 
 export default config;
--- 17.nft-card/src/pages/index.tsx
+++ 17.nft-card/src/pages/index.tsx
@@ -2,13 +2,16 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { useAmp } from 'next/amp';
 import fs from 'fs';
+import path from 'path';
 
 export const config = { amp: 'hybrid' };
+
+const cssPath = path.join(process.cwd(), 'src', 'styles', 'output.css');
+const ampCss = fs.existsSync(cssPath) ? fs.readFileSync(cssPath, 'utf8') : '';
 
 export async function getStaticProps() {
   const res = await fetch('http://www.randomnumberapi.com/api/v1.0/random?min=1000&max=10000');
   const code = await res.json();
-  const ampCss = fs.existsSync("../styles/output.css") ? fs.readFileSync("../styles/output.css", 'utf8') : 'body {background-color: #ddd;}';
   return {
     props: { code, ampCss  },
     revalidate: 10,
@@ -30,39 +33,40 @@ export default function Home({ code, ampCss }: { code: number[], ampCss: string;
         )}
       </Head>
       {isAmp ? (
-        <div className="body">
-          <div className="card">
-            <header className="card__header card__header-overlay">
-              <amp-img src="/images/equilibrium.jpg" alt="equilibrium" width="320" height="320"layout="responsive" className="card__header__image"></amp-img>
-              <div className="card__header-overlay__icon">
-                <amp-img src="/icons/view.svg" alt="view" width="48" height="48" className=""></amp-img>
-              </div>
-            </header>
-            <main className="card__main">
-              <section className="card__main__section">
-                <amp-fit-text width="320" height="60" layout="responsive">
-                  <h1 className="card__title">Equilibrium #{code?.[0]}</h1>
-                </amp-fit-text>
-                <p className="card__description">Our Equilibrium collection promotes balance and calm.</p>
-              </section>
-              <div className="card__main__details">
-                <div className="card__details__detail card__details__detail-bold">
-                  <amp-img src="/icons/ethereum.svg" alt="ethereum" width="12" height="20" className="card__details__icon"></amp-img>
-                  <p>0.041 ETH</p>
+        <>
+          <div className="body">
+            <div className="card">
+              <header className="card__header card__header-overlay">
+                <amp-img src="/images/equilibrium.jpg" alt="equilibrium" width="320" height="320"layout="responsive" className="card__header__image"></amp-img>
+                <div className="card__header-overlay__icon">
+                  <amp-img src="/icons/view.svg" alt="view" width="48" height="48" className=""></amp-img>
                 </div>
-                <div className="card__details__detail">
-                  <amp-img src="/icons/clock.svg" alt="clock" width="18" height="18" className="card__details__icon card__detail__icon-square"></amp-img>
-                  <p className="card__details__title-blue">3 days left</p>
+              </header>
+              <main className="card__main">
+                <section className="card__main__section">
+                  <amp-fit-text width="320" height="60" layout="responsive">
+                    <h1 className="card__title">Equilibrium #{code?.[0]}</h1>
+                  </amp-fit-text>
+                  <p className="card__description">Our Equilibrium collection promotes balance and calm.</p>
+                </section>
+                <div className="card__main__details">
+                  <div className="card__details__detail card__details__detail-bold">
+                    <amp-img src="/icons/ethereum.svg" alt="ethereum" width="12" height="20" className="card__details__icon"></amp-img>
+                    <p>0.041 ETH</p>
+                  </div>
+                  <div className="card__details__detail">
+                    <amp-img src="/icons/clock.svg" alt="clock" width="18" height="18" className="card__details__icon card__detail__icon-square"></amp-img>
+                    <p className="card__details__title-blue">3 days left</p>
+                  </div>
                 </div>
-              </div>
-            </main>
-            <footer className="card__footer">
-              <amp-img src="/images/avatar.jpg" alt="avatar" width="40" height="40" className="card__footer__avatar"></amp-img>
-              <p className="card__footer__description">Creation of <span className="card__footer__author">Arash Asghari</span></p>
-            </footer>
-            <p>{ampCss}</p>
+              </main>
+              <footer className="card__footer">
+                <amp-img src="/images/avatar.jpg" alt="avatar" width="40" height="40" className="card__footer__avatar"></amp-img>
+                <p className="card__footer__description">Creation of <span className="card__footer__author">Arash Asghari</span></p>
+              </footer>
+            </div>
           </div>
-        </div>
+        </>
       ) :
         <div className='body'>
           <div className='card'>
@@ -95,7 +99,7 @@ export default function Home({ code, ampCss }: { code: number[], ampCss: string;
           </div>
         </div> 
       }
-      <style jsx global>{ampCss}</style>
+      {isAmp && <style jsx global>{ampCss}</style>}
     </>
   );
 }
