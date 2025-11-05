02b5b6578fd58c8e2767ecd3cf4cae547b2cf5aa
Arash44 <arashasghari1245@gmail.com>
2025-11-05 11:08:37 +0330

    initiallize project 18

---
+++ 18.loopstudio-landing/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Alata, Josefin_Sans } from "next/font/google";
+import "../styles/globals.css";
+
+const alata = Alata({
+  variable: "--font-alata",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const josefinSans = Josefin_Sans({
+  variable: "--font-josefin-sans",
+  subsets: ["latin"],
+  weight: "300",
+});
+
+export const metadata: Metadata = {
+    title: "18.loopstudio-landing-page | frontend-mentor",
+    description: "18th task from frontend-mentor, developed using next.js framework",
+    authors: [{name: "Arash Asghari"}],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${alata.variable} ${josefinSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
